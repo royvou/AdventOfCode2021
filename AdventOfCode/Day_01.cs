227d439097683e5004840aeee19886c112f8c745
Roy <royvou@hotmail.com>
2021-12-01 20:25:23 +0100

    Day 1 - Part 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -5,14 +5,26 @@ namespace AdventOfCode
     public class Day_01 : BaseDay
     {
         private readonly string _input;
+        private readonly List<int> _numberList;
 
         public Day_01()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllText(InputFilePath);            
+            _numberList = _input.Split("\n").Select(int.Parse).ToList();
+
         }
 
-        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
-
+        public override ValueTask<string> Solve_1() => new(Solve_1_Sync());
+        
+        private string Solve_1_Sync()
+        {
+            return _numberList.Aggregate<int, (int Count, int? Previous)>((Count: 0, Previous: null), (acc, current) =>
+            {
+                if (acc.Previous.HasValue && acc.Previous < current)
+                    return (acc.Count + 1, current);
+                return (acc.Count, current);
+            }).Count.ToString();
+        }
         public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
     }
 }
