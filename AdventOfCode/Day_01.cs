05f122c9d0f890453f1ccb935befd6c728afba38
Roy <royvou@hotmail.com>
2021-12-01 20:43:05 +0100

    Day 1 - Part 2

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,30 +1,42 @@
-﻿using AoCHelper;
+﻿namespace AdventOfCode;
 
-namespace AdventOfCode
+public class Day_01 : BaseDay
 {
-    public class Day_01 : BaseDay
+    private readonly string _input;
+    private readonly List<int> _numberList;
+
+    public Day_01()
     {
-        private readonly string _input;
-        private readonly List<int> _numberList;
-
-        public Day_01()
-        {
-            _input = File.ReadAllText(InputFilePath);            
-            _numberList = _input.Split("\n").Select(int.Parse).ToList();
-
-        }
-
-        public override ValueTask<string> Solve_1() => new(Solve_1_Sync());
-        
-        private string Solve_1_Sync()
-        {
-            return _numberList.Aggregate<int, (int Count, int? Previous)>((Count: 0, Previous: null), (acc, current) =>
-            {
-                if (acc.Previous.HasValue && acc.Previous < current)
-                    return (acc.Count + 1, current);
-                return (acc.Count, current);
-            }).Count.ToString();
-        }
-        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        _input = File.ReadAllText(InputFilePath);
+        _numberList = _input.Split("\n").Select(int.Parse).ToList();
     }
+
+    public override ValueTask<string> Solve_1() => new(Solve_1_Sync());
+
+    private string Solve_1_Sync()
+        => _numberList.Aggregate<int, (int Count, int? Previous)>((Count: 0, Previous: null), (acc, current) =>
+        {
+            if (acc.Previous.HasValue && acc.Previous < current)
+            {
+                return (acc.Count + 1, current);
+            }
+
+            return (acc.Count, current);
+        }).Count.ToString();
+
+    public override ValueTask<string> Solve_2() => new(Solve_2_Sync());
+
+    private string Solve_2_Sync()
+        => _numberList.Select((_, index) => GeListForIndex(index)).Where(x => x.Length == 3).Aggregate<int[], (int Count, int[]? Previous)>((Count: 0, Previous: null), (acc, current) =>
+        {
+            if (acc.Previous != default && acc.Previous.Sum() < current.Sum())
+            {
+                return (acc.Count + 1, current);
+            }
+
+            return (acc.Count, current);
+        }).Count.ToString();
+
+    private int[] GeListForIndex(int index)
+        => _numberList.Skip(index).Take(3).ToArray();
 }
