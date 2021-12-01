cea75e17231e04b5dc012a699dca493969af99a3
Roy <royvou@hotmail.com>
2021-12-01 20:49:21 +0100

    Add improved support for dotnet watch

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,16 +1,22 @@
-﻿using AoCHelper;
-
-if (args.Length == 0)
+﻿if (Environment.GetEnvironmentVariable("DOTNET_WATCH") != default)
 {
     Solver.SolveLast();
+    return;
 }
-else if (args.Length == 1 && args[0].Contains("all", System.StringComparison.CurrentCultureIgnoreCase))
-{
-    Solver.SolveAll();
-}
-else
-{
-    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
 
-    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+switch (args.Length)
+{
+    case 0:
+        Solver.SolveLast();
+        break;
+    case 1 when args[0].Contains("all", System.StringComparison.CurrentCultureIgnoreCase):
+        Solver.SolveAll();
+        break;
+    default:
+    {
+        var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+
+        Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+        break;
+    }
 }
