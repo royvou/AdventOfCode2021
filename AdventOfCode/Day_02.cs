0a04b192eecf36af1d2dc076a4e1a506f27ab5c7
Roy <royvou@hotmail.com>
2021-12-02 22:47:02 +0100

    Day 2 - Refactor

--- AdventOfCode/Day_02.cs
+++ AdventOfCode/Day_02.cs
@@ -2,6 +2,13 @@
 
 public class Day_02 : BaseDay
 {
+    public enum Day02Direction
+    {
+        forward,
+        down,
+        up,
+    }
+
     private readonly string _input;
     private readonly Day02Data[] _inputData;
 
@@ -20,42 +27,36 @@ public class Day_02 : BaseDay
 
     private string Solve_1_Sync()
     {
-        var (x, y) = _inputData.Aggregate((X: 0, Y: 0), (position, current) =>
-        {
-            return current.Direction switch
+        var (x, y, aim) = _inputData.Aggregate(new Day02Position(0, 0, 0), (position, current) =>
+            current.Direction switch
             {
-                Day02Direction.down => (position.X, position.Y + current.Amount),
-                Day02Direction.forward => (position.X + current.Amount, position.Y),
-                Day02Direction.up => (position.X, position.Y - current.Amount),
-            };
-        });
+                Day02Direction.down => position with { Y = position.Y + current.Amount, },
+                Day02Direction.up => position with { Y = position.Y - current.Amount, },
+                Day02Direction.forward => position with { X = position.X + current.Amount, },
+            });
         return (x * y).ToString();
     }
 
     public override ValueTask<string> Solve_2() => new(Solve_2_Sync());
-    
+
     private string Solve_2_Sync()
     {
-        var (x, y, aim) = _inputData.Aggregate(new Day02Position(0,0,0), (position, current) =>
-        {
-            return current.Direction switch
+        var (x, y, aim) = _inputData.Aggregate(new Day02Position(0, 0, 0), (position, current) =>
+            current.Direction switch
             {
-                Day02Direction.down => position with { Aim = position.Aim + current.Amount },
-                Day02Direction.up => position with { Aim = position.Aim - current.Amount },
-                Day02Direction.forward => position with { X = position.X + current.Amount, Y = position.Y + ( position.Aim * current.Amount)},
-                
-            };
-        });
+                Day02Direction.down => position with { Aim = position.Aim + current.Amount, },
+                Day02Direction.up => position with { Aim = position.Aim - current.Amount, },
+                Day02Direction.forward => position with { X = position.X + current.Amount, Y = position.Y + position.Aim * current.Amount, },
+            });
+
         return (x * y).ToString();
     }
 
-    public enum Day02Direction
+    public record Day02Data(Day02Direction Direction, int Amount)
     {
-        forward,
-        down,
-        up,
     }
-    public record Day02Data(Day02Direction Direction, int Amount){}
-    
-    public record Day02Position(long X, long Y, long Aim){}
+
+    public record Day02Position(long X, long Y, long Aim)
+    {
+    }
 }
