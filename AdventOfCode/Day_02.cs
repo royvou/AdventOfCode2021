d3c0cc8ad1d97ef3e1203d8df8040332b4b878f0
Roy <royvou@hotmail.com>
2021-12-02 22:27:18 +0100

    Day 2 - Part 1

---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode;
+
+public class Day_02 : BaseDay
+{
+    private readonly string _input;
+    private readonly Day02Data[] _inputData;
+
+    public Day_02()
+    {
+        _input = File.ReadAllText(InputFilePath);
+
+        _inputData = _input.SplitNewLine().Select(line =>
+        {
+            var data = line.Split(' ');
+            return new Day02Data(Enum.Parse<Day02Direction>(data[0]), int.Parse(data[1]));
+        }).ToArray();
+    }
+
+    public override ValueTask<string> Solve_1() => new(Solve_1_Sync());
+
+    private string Solve_1_Sync()
+    {
+        var (x, y) = _inputData.Aggregate((X: 0, Y: 0), (position, current) =>
+        {
+            return current.Direction switch
+            {
+                Day02Direction.down => (position.X, position.Y + current.Amount),
+                Day02Direction.forward => (position.X + current.Amount, position.Y),
+                Day02Direction.up => (position.X, position.Y - current.Amount),
+            };
+        });
+        return (x * y).ToString();
+    }
+
+    public override ValueTask<string> Solve_2() => new(String.Empty);
+
+    public enum Day02Direction
+    {
+        forward,
+        down,
+        up,
+    }
+    public record Day02Data(Day02Direction Direction, int Amount){}
+}
---
+++ AdventOfCode/StringExtensions.cs
@@ -0,0 +1,36 @@
+namespace AdventOfCode;
+
+public static class StringExtensions
+{
+    public static int AsInt(this string input)
+        => int.Parse(input);
+
+    public static int AsInt(this char input)
+        => (int) char.GetNumericValue(input);
+
+
+    public static IEnumerable<int> AsInt(this IEnumerable<string> input)
+        => input.Select(x => int.Parse(x));
+
+    public static IEnumerable<int> ParseAsArray(this string input)
+        => input.SplitNewLine().AsInt();
+
+    public static IEnumerable<long> ParseAsLongArray(this string input)
+        => input.SplitNewLine().Select(x => long.Parse(x));
+
+    public static string[] SplitSpace(this string input)
+        => input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    public static string[] SplitNewLine(this string input)
+        => input.Split(new[]
+        {
+            Environment.NewLine,
+            "\n" //Used as newline in unit tests
+        }, StringSplitOptions.RemoveEmptyEntries);
+
+    private static readonly string doubleNewLine = Environment.NewLine + Environment.NewLine;
+    private static readonly string doubleNewLineCommand = "\n\n";
+
+    public static string[] SplitDoubleNewLine(this string input)
+        => input.Split(new[] {doubleNewLine, doubleNewLineCommand}, StringSplitOptions.RemoveEmptyEntries);
+}
