ca8f7664e8271e5ce6e875dec9e50717bd02714e
Roy <royvou@hotmail.com>
2021-12-03 23:03:30 +0100

    Day 3 - Part 2

--- AdventOfCode/Day_03.cs
+++ AdventOfCode/Day_03.cs
@@ -3,36 +3,101 @@
 public class Day_03 : BaseDay
 {
     private readonly string _input;
-    private readonly int[][] _parsedInput;
+    private readonly int[][] _parsedInputPart1;
+    private readonly int[] _parsedInputPart2;
 
     public Day_03()
     {
         _input = File.ReadAllText(InputFilePath);
+        _parsedInputPart1 = _input.SplitNewLine().Select(x => x.Select(c => c.AsInt()).ToArray()).ToArray();
 
-        _parsedInput = _input.SplitNewLine().Select(x => x.Select(c => c.AsInt()).ToArray()).ToArray();
+        _parsedInputPart2 = _input.SplitNewLine().Select(x => Convert.ToInt32(x, 2)).ToArray();
     }
 
     public override ValueTask<string> Solve_1() => new(Solve_1_Sync());
 
     private string Solve_1_Sync()
     {
-        var result = Enumerable.Range(0, _parsedInput[0].Length).Aggregate(new Day03Record(0, 0), (acc, current) =>
+        var result = Enumerable.Range(0, _parsedInputPart1[0].Length).Aggregate(new Day03Part1Record(0, 0), (acc, current) =>
         {
             var gamma = acc.Gamma;
             gamma <<= 1;
-            var orderByDescending = _parsedInput.Select(x => x[current]).GroupBy(x => x).OrderByDescending(x => x.Count());
-            gamma ^= orderByDescending.First().Key;
+            var orderByDescending = _parsedInputPart1.Select(x => x[current]).GroupBy(x => x).OrderByDescending(x => x.Count()).ToList();
+            gamma ^= orderByDescending[0].Key;
             var epsilon = acc.Epsilon;
             epsilon <<= 1;
-            epsilon ^= orderByDescending.Skip(1).First().Key;
+            epsilon ^= orderByDescending[1].Key;
 
-            return new Day03Record(gamma, epsilon);
+            return new Day03Part1Record(gamma, epsilon);
         });
         return (result.Epsilon * result.Gamma).ToString();
     }
 
 
-    public override ValueTask<string> Solve_2() => new(string.Empty);
+    public override ValueTask<string> Solve_2() => new(Solve_2_Sync());
 
-    public record Day03Record(int Gamma, int Epsilon);
+    private string Solve_2_Sync()
+        => (CalculateOxygenRating() * CalculateCo2Rating()).ToString();
+
+    private int CalculateOxygenRating()
+    {
+        var bitLength = _parsedInputPart1[0].Length;
+
+        var validLines = new List<int>(_parsedInputPart2);
+        for (var i = bitLength - 1; i >= 0; i--)
+        {
+            var (one, zero) = CountNum(validLines, i);
+            if (one >= zero)
+            {
+                validLines.RemoveAll(line => (line & (1 << i)) == 0);
+            }
+            else
+            {
+                validLines.RemoveAll(line => (line & (1 << i)) != 0);
+            }
+
+            if (validLines.Count == 1)
+            {
+                return validLines[0];
+            }
+        }
+
+        return -1;
+    }
+
+    private int CalculateCo2Rating()
+    {
+        var bitLength = _parsedInputPart1[0].Length;
+
+        var validLines = new List<int>(_parsedInputPart2);
+        for (var i = bitLength - 1; i >= 0; i--)
+        {
+            var (one, zero) = CountNum(validLines, i);
+            if (one < zero)
+            {
+                validLines.RemoveAll(line => (line & (1 << i)) == 0);
+            }
+            else
+            {
+                validLines.RemoveAll(line => (line & (1 << i)) != 0);
+            }
+
+            if (validLines.Count == 1)
+            {
+                return validLines[0];
+            }
+        }
+
+        return -1;
+    }
+
+    private (int One, int Zero) CountNum(List<int> validLines, int i)
+        => validLines.Aggregate((One: 0, Zero: 0), (acc, curr) =>
+            (curr & (1 << i)) != 0
+                ? (acc.One += 1, acc.Zero)
+                : (acc.One, acc.Zero += 1));
+
+    public record Day03Part1Record(int Gamma, int Epsilon);
+
+    public record Day03Part2Record(int Gamma, int Epsilon);
 }
