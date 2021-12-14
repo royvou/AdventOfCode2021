2800bc03141e749f14f5d7ed9ed3dee0acf95df0
Roy <royvou@hotmail.com>
2021-12-14 20:31:43 +0100

    Day 14 - Part 1

---
+++ AdventOfCode/Day_14.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode;
+
+public class Day_14 : BaseDay
+{
+    private readonly char[] _initialList;
+    private readonly string _input;
+    private readonly Dictionary<(char, char), char> _insertionMapping;
+
+    public Day_14()
+    {
+        _input = File.ReadAllText(InputFilePath);
+        var splitGroups = _input.SplitDoubleNewLine();
+
+        _initialList = splitGroups[0].ToCharArray();
+
+        _insertionMapping = splitGroups[1].SplitNewLine().ToDictionary(x => (x[0], x[1]), y => y[^1]);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var list = new LinkedList<char>(_initialList);
+
+        for (int i = 0; i < 10; i++)
+        {
+            var current = list.First;
+            while (current != default && current.Next != default)
+            {
+                var next = current.Next;
+
+                if (_insertionMapping.TryGetValue((current.Value, next.Value), out var toInsert))
+                {
+                    list.AddAfter(current, toInsert);
+                }
+
+
+                current = next;
+            }
+
+        }
+        
+        var grouping = list.GroupBy(x => x);
+        return new ValueTask<string>((grouping.Max(x => x.Count()) - grouping.Min(x => x.Count())).ToString());
+    }
+
+    public override ValueTask<string> Solve_2() => new(string.Empty);
+}
